27bdcddcb5eac06c21a516cd4524f79025f10213
Hollman Rojas <hollmanerojas@gmail.com>
2025-02-23 14:16:49 -0500

    Feature/Instalation of flutter_riverpod and configuration into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:marvel_app/config/router/app_router.dart';
 import 'config/theme/app_theme.dart';
@@ -6,7 +7,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
