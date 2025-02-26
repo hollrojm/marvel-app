862b0fb8d72e453155ddcb7247f5a7869e89fae4
Hollman Rojas <hollmanerojas@gmail.com>
2025-02-26 00:41:58 -0500

    Feature/add datasourseimpl and repositoryimpl

--- lib/infrastructure/datasources/movie_datasource_impl.dart
+++ lib/infrastructure/datasources/movie_datasource_impl.dart
@@ -3,6 +3,9 @@ import 'package:dio/dio.dart';
 import 'package:marvel_app/config/constants/environment.dart';
 import 'package:marvel_app/domain/datasources/movies_datasourse.dart';
 import 'package:marvel_app/domain/entities/movie_entity.dart';
+import 'package:marvel_app/infrastructure/mappers/marvel_movie_mapper.dart';
+import 'package:marvel_app/infrastructure/models/marvel_movies/marvel_movies_model.dart';
+import 'package:marvel_app/infrastructure/models/marvel_movies/marvel_movies_response.dart';
 
 class MarvelMovieDatasourceImpl extends MarvelMoviesDatasourse {
   final dio = Dio(
@@ -20,7 +23,13 @@ class MarvelMovieDatasourceImpl extends MarvelMoviesDatasourse {
   @override
   Future<List<MarvelMovieEntity>> getMarvelMovies({int page = 1}) async {
     final response = await dio.get('/discover/movie');
-    final List<MarvelMovieEntity> movies = [];
+    final marvelMoviesResponse = MarvelMoviesResponse.fromJson(response.data);
+    final List<MarvelMovieEntity> movies = marvelMoviesResponse.results
+        .map(
+          (marvelMovies) => MarvelMovieMapper.mavelMovieToEntity(marvelMovies),
+        )
+        .toList();
+
     return movies;
   }
 }
