2fb8ba3e7dd1d8ecaaf23b745fdab1cf58f7044a
Hollman Rojas <hollmanerojas@gmail.com>
2025-02-25 21:18:24 -0500

    Fix/Refactor movies_datatsourse

--- lib/domain/datasources/comics_datasourse.dart
+++ lib/domain/datasources/comics_datasourse.dart
@@ -1,5 +1,3 @@
-import 'package:marvel_app/domain/entities/movie_entity.dart';
-
 abstract class ComicsDatasourse {
   Future<List<Map<String, dynamic>>> getComics();
 }
--- lib/domain/datasources/movies_datasourse.dart
+++ lib/domain/datasources/movies_datasourse.dart
@@ -1,5 +1,5 @@
 import 'package:marvel_app/domain/entities/movie_entity.dart';
 
-abstract class MoviesDatasourse {
+abstract class MarvelMoviesDatasourse {
   Future<List<MovieEntity>> getMarvelMovies({int page = 1});
 }
