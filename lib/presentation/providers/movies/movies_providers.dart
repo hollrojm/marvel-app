da45d9b9a442a78ed8f0b6f4db9fcd6c388e9b2b
Hollman Rojas <hollmanerojas@gmail.com>
2025-02-27 00:41:05 -0500

    Feature/Movies Providers

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:marvel_app/domain/entities/movie_entity.dart';
+import 'package:marvel_app/presentation/providers/movies/movies_repository_provider.dart';
+
+final geymoviesProviders =
+    StateNotifierProvider<MoviesNotifier, List<MarvelMovieEntity>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getMovies;
+  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+typedef MovieCallback = Future<List<MarvelMovieEntity>> Function({int page});
+
+class MoviesNotifier extends StateNotifier<List<MarvelMovieEntity>> {
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({
+    required this.fetchMoreMovies,
+  }) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    final List<MarvelMovieEntity> movies =
+        await fetchMoreMovies(page: currentPage);
+    state = [...state, ...movies];
+  }
+}
